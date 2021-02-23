1c92e3f7f28ecca0dd8d975bbe2963a4f37647e6
Fatlum <fgjinofci@gmail.com>
2021-02-23 20:10:36 +0100

    working progress

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mx-auto">
+    <GameContainer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameContainer from './components/GameContainer';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GameContainer 
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: azure;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  .logo h3 {
+    margin: 0;
+  }
+
+  .game-board {
+    border: 5px cadetblue solid;
+
+    width: 350px;
+    margin: 0 auto;
+  }
+
+  .column {
+    cursor: pointer;
+  }
+
+  .column rect.play:hover {
+    fill-opacity: 0.75;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
